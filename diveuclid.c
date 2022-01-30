061d5df885d2ac50b9587b6330ae5ce9f456a72c
cmauget <clement.mauget@gmail.com>
2022-01-30 19:05:11 +0100

    ajout du time

--- diveuclid.c
+++ diveuclid.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include <time.h>
+#include <sys/time.h>
 
 typedef float vect[100];
 
+float time_diff2(struct timespec *start, struct timespec *end)
+{
+  return (end->tv_sec - start->tv_sec) + 1e-9*(end->tv_nsec - start->tv_nsec); //On ajoute la différence des secondes avec la différence de n-ieme de secondes
+}
 
 
-void save( float sum, vect P, float x, int deg )
+void save( float sum, vect P, float x, int deg, float time )
 {
   int i;
   FILE *fichier ;
@@ -18,9 +24,11 @@ void save( float sum, vect P, float x, int deg )
         fprintf(fichier, "Le coefficient de  X^%d  est %f \n",i, P[i]);
      }
 
-  fprintf(fichier,"La valeur du polynome en %f est = %f",x,sum) ;
+  fprintf(fichier,"La valeur du polynome en %f est = %f \n",x,sum) ;
+  fprintf(fichier, "Temps d'éxécution: %0.8f sec\n", time);
   fclose (fichier) ;
 }
+
 
 /*void read_ints ();
   {
@@ -41,6 +49,8 @@ int main(void)
   int i,deg;
   float sum,x;
   vect P;
+  struct timespec start; //Heure de début de l'execution
+  struct timespec end; //Heure de fin de l'éxécution
 
   printf("Entrez le deg du poly =  ");
   scanf("%d",&deg);
@@ -57,6 +67,7 @@ int main(void)
   /* deg = 5; //Entrez les deg de P
   P  = {1,-3,0,2,0,-13}; // Entrez les alpha du polynome ici
   x= 3; //entrez la valuer ou évaluer le polynôme */
+  clock_gettime(CLOCK_REALTIME, &start); //On récupère l'heure de début
 
   for( i=deg; i>0; i--)
   {
@@ -64,6 +75,11 @@ int main(void)
   }
 
   sum=sum+P[0];
+
+  clock_gettime(CLOCK_REALTIME, &end); //On récupère l'heure de fin
+
   printf("La valuer du polynome est = %f \n",sum);
-  save(sum,P,x,deg);
+  printf("Temps d'éxécution: %0.8f sec\n",time_diff2(&start,&end)); //Affichage du temps d'éxécution
+
+  save(sum,P,x,deg,time_diff2(&start,&end));
 }
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Bienvenue dans la partie C du code.
+
+Malheureusement il n'a pas été possible de lire les valeurs d'un fichier d'entré.
+Cependant les valeurs du degré du polynôme, de ses coefficients et de la valeur
+ou étudier le polynôme sera demandé dans le programme.
+
+Les donnés ainsi que le résultat est enregistré dans le fichier valpolyR.txt.
+Le temps d’exécution du programme est aussi évalué, affiché et enregistré dans le fichier
+
+Programme en C : diveuclid.c compilé avec gcc et l'option -o | l’exécutable diveuclid est a jour.
